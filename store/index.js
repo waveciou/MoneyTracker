37a456da89d3868a2c58929d34291777e4232d4a
丘惟夫 <waveciou@gmail.com>
2021-03-27 00:35:19 +0800

    system development

--- store/index.js
+++ store/index.js
@@ -5,38 +5,6 @@
 //* State
 export const state = () => ({
   accounts: [],
-  // siteName: 'Money Tracker',
-  // pages: {
-  //   name: '',
-  //   head: ''
-  // },
-  // route: [
-  //   {
-  //     routeName: 'index',
-  //     markName: 'account',
-  //     title: '記帳日誌'
-  //   },
-  //   {
-  //     routeName: 'overview',
-  //     markName: 'overview',
-  //     title: '帳戶總覽'
-  //   },
-  //   {
-  //     routeName: 'record',
-  //     markName: 'record',
-  //     title: '新增記帳'
-  //   },
-  //   {
-  //     routeName: 'analysis',
-  //     markName: 'analysis',
-  //     title: '項目分析'
-  //   },
-  //   {
-  //     routeName: 'setting',
-  //     markName: 'setting',
-  //     title: '設定'
-  //   }
-  // ],
   categoriesList: [],
   currentDate: {
     year: 0,
