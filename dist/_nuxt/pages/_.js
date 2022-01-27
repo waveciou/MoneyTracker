53e94d794efdf5ed9976094b69b3fed1e34118f0
丘惟夫 <waveciou@gmail.com>
2022-01-27 20:48:24 +0800

    generate

--- dist/_nuxt/pages/_.js
+++ dist/_nuxt/pages/_.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{302:function(n,e,t){"use strict";t.r(e);var o={asyncData:function(n){n.route,n.query;return(0,n.redirect)("/")}},r=t(7),component=Object(r.a)(o,void 0,void 0,!1,null,null,null);e.default=component.exports}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{303:function(n,e,t){"use strict";t.r(e);var o={asyncData:function(n){n.route,n.query;return(0,n.redirect)("/")}},r=t(7),component=Object(r.a)(o,void 0,void 0,!1,null,null,null);e.default=component.exports}}]);
