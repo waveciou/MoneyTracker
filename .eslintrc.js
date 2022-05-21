74500667aa135060d79a069ecba3bd84b54469d8
丘惟夫 <waveciou@gmail.com>
2022-05-21 14:32:21 +0800

    [dev] update version and linter

---
+++ .eslintrc.js
@@ -0,0 +1,74 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  'parserOptions': {
+    'ecmaVersion': 12
+  },
+  'plugins': [
+    'vue'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'off',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-unused-vars': ['off'],
+    'no-undef': ['off'],
+    'vue/require-default-prop': ['off'],
+    'vue/order-in-components': ['error', {
+      'order': [
+        'el',
+        ['template', 'render'],
+        'renderError',
+        'data',
+        'name',
+        'head',
+        'key',
+        'parent',
+        'functional',
+        ['delimiters', 'comments'],
+        ['components', 'directives', 'filters'],
+        'extends',
+        'mixins',
+        ['provide', 'inject'],
+        'ROUTER_GUARDS',
+        'layout',
+        'middleware',
+        'validate',
+        'scrollToTop',
+        'transition',
+        'loading',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'emits',
+        'setup',
+        'asyncData',
+        'fetch',
+        'LIFECYCLE_HOOKS',
+        'methods',
+        'computed',
+        'watch',
+        'watchQuery'
+      ]
+    }]
+  }
+};
