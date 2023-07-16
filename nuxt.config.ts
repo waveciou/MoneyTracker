9f3fa800e0e5ea3d2a7e1f666a8b62c2f5fb3827
丘惟夫 <waveciou@gmail.com>
2023-07-16 19:47:23 +0800

    [dev] import pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module', '@pinia/nuxt'],
   css: ['@/assets/scss/main.scss'],
 });
