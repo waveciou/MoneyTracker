13546061cb0bc212f33f1010a9245bccb8a4e121
丘惟夫 <waveciou@gmail.com>
2023-07-25 20:41:55 +0800

    [dev] text area component development

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,15 @@
     <InputText v-model="textValue" class="mb-2" />
     <InputNumber v-model.number="numberValue" class="mb-2" />
     <InputClearText v-model="clearTextValue" class="mb-2" />
+    <InputTextArea v-model="textAreaValue" class="mb-2" />
   </div>
 </template>
 
 <script setup lang="ts">
   const textValue = ref<string>('');
-  const clearTextValue = ref<string>('');
   const numberValue = ref<number>(0);
+  const clearTextValue = ref<string>('');
+  const textAreaValue = ref<string>('');
 </script>
 
 <style lang="scss" scoped></style>
