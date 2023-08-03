7d1f695736a99b00e7eccfe55da2545f29f376ed
丘惟夫 <waveciou@gmail.com>
2023-08-03 15:16:55 +0800

    [dev] form note component development

---
+++ components/Form/Note.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <label :for="id" class="block text-base mb-2">備註</label>
+    <InputTextArea
+      :id="id"
+      v-model.trim="contextValue"
+      :placeholder="props.placeholder"
+      :disabled="props.disabled"
+      :readonly="props.readonly"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { watch } from 'vue';
+  import { v4 as uuidv4 } from 'uuid';
+
+  const props = withDefaults(
+    defineProps<{
+      modelValue: string;
+      placeholder?: string;
+      disabled?: boolean;
+      readonly?: boolean;
+    }>(),
+    {
+      modelValue: '',
+      placeholder: '',
+      disabled: false,
+      readonly: false,
+    }
+  );
+
+  const emits = defineEmits<{
+    (e: 'update:modelValue', value: string): void;
+  }>();
+
+  const id = ref<string>(uuidv4());
+  const contextValue = ref<string>(props.modelValue);
+
+  watch(
+    () => contextValue.value,
+    (value: string) => {
+      emits('update:modelValue', value);
+    }
+  );
+</script>
--- pages/record.vue
+++ pages/record.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="w-full">
-    <div>Record</div>
     <FormPrice v-model.number="formPrice" class="mb-4" />
     <FormName v-model.trim="formName" class="mb-4" />
     <FormStore v-model.trim="formStore" class="mb-4" />
+    <FormNote v-model.trim="formNote" class="mb-4" />
   </div>
 </template>
 
@@ -11,6 +11,7 @@
   const formPrice = ref<number>(123);
   const formName = ref<string>('');
   const formStore = ref<string>('');
+  const formNote = ref<string>('');
 </script>
 
 <style lang="scss" scoped></style>
