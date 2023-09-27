fa929352431f1fa8490e6c7bd292e8770ec301c5
丘惟夫 <waveciou@gmail.com>
2023-09-27 21:58:00 +0800

    [dev] overview total price component development

---
+++ components/Overview/TotalPrice.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="py-3 text-2xl text-white text-center truncate">
+    Total
+    <span class="ml-1" :class="providePriceClass">{{ providePrice }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue';
+  import { useRecordStore } from '@/stores/recordStore';
+
+  const recordStore = useRecordStore();
+
+  const providePriceClass = computed((): string => {
+    if (recordStore.totalPrice < 0) {
+      return 'text-red';
+    }
+    return 'text-green';
+  });
+
+  const providePrice = computed((): string => {
+    const price = useFinanceNumber(recordStore.totalPrice);
+
+    if (recordStore.totalPrice > 0) {
+      return `+${price}`;
+    }
+    return price;
+  });
+</script>
--- pages/overview.vue
+++ pages/overview.vue
@@ -1,32 +1,7 @@
 <template>
   <div>
-    <div class="py-3 text-2xl text-white text-center truncate">
-      Total
-      <span class="ml-1" :class="providePriceClass">{{ providePrice }}</span>
-    </div>
+    <OverviewTotalPrice />
   </div>
 </template>
 
-<script setup lang="ts">
-  import { computed } from 'vue';
-  import { useRecordStore } from '@/stores/recordStore';
-
-  const recordStore = useRecordStore();
-
-  const providePriceClass = computed((): string => {
-    if (recordStore.totalPrice < 0) {
-      return 'text-red';
-    }
-    return 'text-green';
-  });
-
-  const providePrice = computed((): string => {
-    const price = useFinanceNumber(recordStore.totalPrice);
-
-    if (recordStore.totalPrice <= 0) {
-      return price;
-    } else {
-      return `+${price}`;
-    }
-  });
-</script>
+<script setup lang="ts"></script>
