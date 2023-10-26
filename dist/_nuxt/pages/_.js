db0f04c1c6393dd3243958ac6a107eba2d108f15
丘惟夫 <waveciou@gmail.com>
2023-10-26 18:55:09 +0800

    [dev] delete dist folder

--- dist/_nuxt/pages/_.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{358:function(n,e,t){"use strict";t.r(e);var r={asyncData:function(n){n.route,n.query;return(0,n.redirect)("/")}},c=t(14),component=Object(c.a)(r,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
